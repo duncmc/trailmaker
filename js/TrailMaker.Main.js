968bed4cbdf7b83b65a9106d5ed5515c455da0da
Duncan McMillan <duncmc@gmail.com>
2014-05-02 20:02:43 +0100

    Adding an updating points now draws a polyline.

--- js/TrailMaker.Main.js
+++ js/TrailMaker.Main.js
@@ -12,8 +12,9 @@ TrailMaker.module('Main', function(Main, App) {
 	Main.trail;
 	Main.point;
 	
-	Main.popup;
 	Main.map;
+	Main.popup;
+	Main.polyline;
 	Main.default_zoom = 13;
 	Main.default_center = [53.738666, -0.333152];
 	
@@ -50,14 +51,19 @@ TrailMaker.module('Main', function(Main, App) {
 		start: function(options){
 			
 			Main.trail = new App.Models.Trail();
+			
 			Main.trail.on('add', function(point, trail, e){
 				var marker = L.marker(point.get('latlng'), { draggable:true }).addTo(App.Main.map);
 				point.set('marker', marker);
 			}).on('change', function(point, e){
 				var trail_list_view = new App.Views.TrailPointListView({ collection:point.collection });
 				App.points_list_region.show(trail_list_view);
+				Main.controller.redrawPath();
+			}).on('sort', function(trail, e){
+				Main.controller.redrawPath();
 			}).on('remove', function(model, trail, e){
 				Main.map.removeLayer(model.get('marker'));
+				Main.controller.redrawPath();
 			});
 			
 			/*
@@ -115,12 +121,14 @@ TrailMaker.module('Main', function(Main, App) {
 			options.zoom = options.zoom || Main.default_zoom;
 			
 			Main.map = L.map(el_id, options);
-			Main.popup = L.popup();
 			
 			L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 			    attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
 			    maxZoom: 18
 			}).addTo(Main.map);
+			
+			Main.popup = L.popup();
+			Main.polyline = L.polyline(Main.trail.pluck('latlng')).addTo(Main.map);
 			
 			Main.map.on('click', this.showLatLng, this);
 		},
@@ -152,7 +160,9 @@ TrailMaker.module('Main', function(Main, App) {
 				.openOn(Main.map);
 		},
 		
-		
+		redrawPath: function() {
+			Main.polyline.setLatLngs(Main.trail.pluck('latlng'));
+		},
 		
 		// -----------------------------------------
 		// UTILITY METHODS
